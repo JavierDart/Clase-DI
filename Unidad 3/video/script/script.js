5d1d054a65aba0d01ead9547e8ac2231c5284f07
JavierDart <javinico08@gmail.com>
2017-12-19 18:38:24 +0100

    19/12/2017

--- Unidad 3/video/script/script.js
+++ Unidad 3/video/script/script.js
@@ -7,19 +7,5 @@ onload = function(){
             document.getElementById('player').muted = true;
           }
         }
-        function camb(){
-          var cancion = document.getElementsByTagName('source');
-          if(cancion.src=="audio/SOUNDPROOF.ogg" || cancion.src=="audio/SOUNDPROOF.mp3"){
-            document.getElementsByTagName('source')[0].src = "audio/VORTTEX.ogg";
-            document.getElementsByTagName('source')[1].src = "audio/VORTTEX.mp3";
-          }
-          if(cancion.src=="audio/VORTTEX.ogg" || cancion.src=="audio/VORTTEX.mp3"){
-            document.getElementsByTagName('source')[0].src = "audio/SOUNDPROOF.ogg";
-            document.getElementsByTagName('source')[1].src = "audio/SOUNDPROOF.mp3";
-          }
-          else {
-
-          }
-        }
-
+        
 }
